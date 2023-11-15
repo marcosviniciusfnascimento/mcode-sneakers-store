695df7cf031501d994fa239b612534ee6cf25270
Marcos Nascimento <marcos.nascimento@avmb-asten.com.br>
2023-11-15 13:42:40 -0300

    try

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
-  plugins: [],
-};
-export default config;
+module.exports = {
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+	],
+	plugins: [],
+}
